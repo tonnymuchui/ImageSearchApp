3d33bc6d342793ce4d0d8fe26d9f668349246eaf
tonnymuchui <tonykanyingah@gmail.com>
2023-01-15 22:30:17 +0300

    Add Api & search function

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/api/UnsplashResponse.kt
@@ -0,0 +1,8 @@
+package com.codinginflow.imagesearchapp.api
+
+import com.codinginflow.imagesearchapp.data.UnsplashPhoto
+
+data class UnsplashResponse(
+    val results: List<UnsplashPhoto>
+) {
+}
