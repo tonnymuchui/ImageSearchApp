93fde7a17a75757c8f66265e970dd05df7b8ebc9
tonnymuchui <tonykanyingah@gmail.com>
2023-01-17 12:40:59 +0300

    PagingSource & PagingData

--- app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
@@ -1,7 +1,19 @@
 package com.codinginflow.imagesearchapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.codinginflow.imagesearchapp.api.UnsplashApi
 import javax.inject.Inject
 
-class UnsplashRepository @Inject constructor(private val url: UnsplashApi) {
+class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {UnsplashPagingSource(unsplashApi, query)}
+        ).liveData
 }
--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,19 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.codinginflow.imagesearchapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.photos.observe(viewLifecycleOwner){
+
+        }
+    }
 }
