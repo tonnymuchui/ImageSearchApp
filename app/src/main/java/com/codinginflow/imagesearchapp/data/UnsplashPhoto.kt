67d12567854931a7917e34b4df294f6dede06f06
tonnymuchui <tonykanyingah@gmail.com>
2023-01-18 23:39:24 +0300

    Api access key

--- app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashPhoto.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashPhoto.kt
@@ -9,21 +9,22 @@ data class UnsplashPhoto(
     val description: String?,
     val urls: UnsplashPhotoUrls,
     val user: UnsplashUser
-): Parcelable {
+) : Parcelable {
+
     @Parcelize
     data class UnsplashPhotoUrls(
         val raw: String,
         val full: String,
         val regular: String,
-        val small:String,
+        val small: String,
         val thumb: String,
-    ): Parcelable
+    ) : Parcelable
 
     @Parcelize
     data class UnsplashUser(
         val name: String,
-        val username: String,
-    ): Parcelable {
+        val username: String
+    ) : Parcelable {
         val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=referral"
     }
 }
