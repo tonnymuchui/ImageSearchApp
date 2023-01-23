e83e8aa0b75d20afa83f7ecb8da29b04539771de
tonnymuchui <tonykanyingah@gmail.com>
2023-01-23 23:24:57 +0300

    Fixing Api

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -8,11 +8,20 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.codinginflow.imagesearchapp.data.UnsplashRepository
 
-class GalleryViewModel @ViewModelInject constructor(private val repository: UnsplashRepository) :
-    ViewModel() {
+class GalleryViewModel @ViewModelInject constructor(
+    private val repository: UnsplashRepository
+) : ViewModel() {
+
     private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
     val photos = currentQuery.switchMap { queryString ->
-    repository.getSearchResults(queryString).cachedIn(viewModelScope)}
+        repository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
+    }
+
     companion object {
         private const val DEFAULT_QUERY = "cats"
     }
